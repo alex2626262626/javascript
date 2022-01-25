98f56d95c75a028b7c62e9aa39e61a4a62ed29b1
alex2626262626 <86064908+alex2626262626@users.noreply.github.com>
2022-01-25 14:52:24 +0100

    first commit

---
+++ jour-1/jour01.js
@@ -0,0 +1,28 @@
+// exo 1
+console.log('hello world')
+//exo 2
+var test="my name is alexandre"
+console.log(test)
+// exo 3
+var name="alexandre"
+console.log("nice to meet you",name)
+// exo 4
+var testLenght="I'm very long"
+console.log(testLenght.length)
+// exo 5
+food="croissant is meh"
+food=food.replace("meh","so good")
+console.log(food)
+// exo 6
+var basic="This is cool"
+var basicUp=basic.toUpperCase()
+var basicDown=basic.toLowerCase()
+console.log(basic,basicUp,basicDown)
+// exo 7
+var word="banana"
+word=word.split("")
+console.log(word)
+// exo 8
+var age="27"
+var template="I'm" + " " + age +" "+ "years old"
+console.log(template)
