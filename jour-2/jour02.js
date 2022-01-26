803f33b68400049c1c2f65fc342343bc28db079b
alex2626262626 <86064908+alex2626262626@users.noreply.github.com>
2022-01-26 15:01:24 +0100

    create second commit

--- jour-2/jour02.js
+++ jour-2/jour02.js
@@ -6,3 +6,49 @@ console.log(integer,"\n",float)
 var basic=34
 var stringified=basic.toString()
 console.log(stringified)
+// exo 3
+var num=1.5
+var rounded=Math.round(num)
+console.log(rounded)
+// exo 4
+var test=5
+var bis=12
+console.log(test+bis)
+console.log(test-bis)
+console.log(test*bis)
+console.log(test/bis)
+console.log(test**bis)
+// exo 5
+console.log(test<bis)
+console.log(test>bis)
+console.log(test>=bis)
+console.log(test<=bis)
+console.log(test===bis)
+console.log(test!==bis)
+// exo 6
+var score=20
+var limit=50
+if(score>=limit){
+    console.log("ok good")
+}
+else{
+    console.log("oh no")
+}
+// exo 7
+var password="azerty"
+if((password.length)<=5){
+    console.log('the password is not secure')
+}
+else{
+    console.log('the password is secure')
+}
+// exo 8
+if(score>=limit && (password.length)>5){
+    console.log("everything is ok")
+}
+else if(score>=limit || (password.length)>5){
+    console.log("something is ok") 
+}
+else{
+    console.log("nothing is ok")
+}
