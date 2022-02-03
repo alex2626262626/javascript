0c86a3f41ea7e247cbec16e660d1de30d32bffe2
alex2626262626 <86064908+alex2626262626@users.noreply.github.com>
2022-02-03 14:11:56 +0100

    create commit

---
+++ jour-7/jour07.js
@@ -0,0 +1,10 @@
+// exo 1
+var fs=require('fs')
+fs.readFile("jour07.txt", function (err, data) {
+   if (err) {
+      console.error(err);
+      return;
+   }
+
+   console.log("Lecture en différé : " + data.toString());
+});
